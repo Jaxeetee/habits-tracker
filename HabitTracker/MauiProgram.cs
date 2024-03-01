87cf342ca455f5976e586bf5686aeea0de3201bd
jacob <Jacob@projactivity.com>
2024-03-01 11:36:40 +1100

    added project to repo

---
+++ HabitTracker/MauiProgram.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace HabitTracker
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Manrope-Bold.ttf", "ManropeBold");
+                    fonts.AddFont("Manrope-ExtraBold.ttf", "ManropeExtraBold");
+                    fonts.AddFont("Manrope-ExtraLight.ttf", "ManropeExtraLight");
+                    fonts.AddFont("Manrope-Light.ttf", "ManropeLight");
+                    fonts.AddFont("Manrope-Medium.ttf", "ManropeMedium");
+                    fonts.AddFont("Manrope-Regular.ttf", "ManropeRegular");
+                    fonts.AddFont("Manrope-SemiBold.ttf", "ManropeSemiBold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
