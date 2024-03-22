f68d34656982146856f242a44bfd052029aabf8b
Jaxeetee <jacob.esclamado19@gmail.com>
2024-03-23 00:32:17 +1100

    added row for add button and other future buttons

--- HabitTracker/MainPage.xaml.cs
+++ HabitTracker/MainPage.xaml.cs
@@ -2,12 +2,20 @@
 {
     public partial class MainPage : ContentPage
     {
+        private int count = 0;
 
         public MainPage()
         {
             InitializeComponent();
 
 
+        }
+
+        public void ButtonAddHabit(object sender, EventArgs e)
+        {
+            count++;
+            btn_add.Text = $"clicked {count} times";
+            Console.WriteLine("Clicked!");
         }
 
 
