5f8ae34c8892eee921f6fd8c72da2ebc163663cd
Jaxeetee <jacob.esclamado19@gmail.com>
2024-03-26 00:10:50 +1100

    refactored the add button in home page

--- HabitTracker/MainPage.xaml.cs
+++ HabitTracker/MainPage.xaml.cs
@@ -2,22 +2,15 @@
 {
     public partial class MainPage : ContentPage
     {
-        private int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
-
-
         }
 
-        public void ButtonAddHabit(object sender, EventArgs e)
-        {
-            count++;
-            btn_add.Text = $"clicked {count} times";
-            Console.WriteLine("Clicked!");
-            Navigation.PushAsync(new AddPage());
-        }
+        public void ButtonAddHabit(object sender, EventArgs e) => Navigation.PushAsync(new AddPage());
+
+
+        
         
 
     }
